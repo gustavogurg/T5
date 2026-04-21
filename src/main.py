7f15e8fe9c67d04e448ec73adf85071761cc96d3
gustavogurg <gustavogurgel@edu.unifor.br>
2026-04-21 17:31:49 -0300

    fixes import algs4

---
+++ src/main.py
@@ -0,0 +1,44 @@
+from algs4.graph import Graph
+
+
+file_path ='../dados/brasil.txt'
+
+def read_graph(file_path):
+    with open(file_path, 'r') as f:
+        V = int(f.readline().strip())
+        E = int(f.readline().strip())
+
+        G = Graph(V)
+
+        edges_read = 0
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+
+            parts = line.split()
+            if len(parts) < 2:
+                continue
+
+            v, w = map(int, parts[:2])
+            G.add_edge(v, w)
+            edges_read += 1
+
+            if edges_read == E:
+                break
+    return G
+
+def printGraph(G):
+    print(f"{G.V} vertices, {G.E} arestas")
+    for v in range(G.V):
+        print(f"{v} ->", end=" ")
+        node = G.adj[v].first
+        while node is not None:
+            print(node.item, end=" ")
+            node = node.next
+        print()
+
+G = read_graph(file_path)
+printGraph(G)
+
+
